3c3795d9c783bd9360bd649737a94b97cd3e2e5e
FerCamarena <contacto.fernandoc@gmail.com>
2025-07-03 20:51:27 -0600

    Update: Audio manager initialization
    Update Audio manager to allow Awake call to read stored previous volumes and update each channel volume.

--- Assets/_Scripts/Managers/Global/_AudioManager.cs
+++ Assets/_Scripts/Managers/Global/_AudioManager.cs
@@ -27,7 +27,20 @@ namespace App.Game.Managers {
             Events.Settings.OnAtmosphereVolumeUpdated -= UpdateAtmosphereChannelVolume;
             Events.Settings.OnVoiceVolumeUpdated -= UpdateVoiceChannelVolume;
         }
+    
+        private void Awake() {
+            this.SetStoredVolume();
+        }
     // ? CUSTOM METHODS=============================================================================================================================
+        private void SetStoredVolume() {
+            this.UpdateMasterChannelVolume(PlayerPrefs.GetFloat("masterVolume", 0.5f));
+            this.UpdateMusicChannelVolume(PlayerPrefs.GetFloat("musicVolume", 0.5f));
+            this.UpdateUIChannelVolume(PlayerPrefs.GetFloat("uiVolume", 0.5f));
+            this.UpdateSFXChannelVolume(PlayerPrefs.GetFloat("sfxVolume", 0.5f));
+            this.UpdateAtmosphereChannelVolume(PlayerPrefs.GetFloat("atmosphereVolume", 0.5f));
+            this.UpdateVoiceChannelVolume(PlayerPrefs.GetFloat("voiceVolume", 0.5f));
+        }
+
         private void UpdateMasterChannelVolume(float newValue) => this.MasterMixer.SetFloat("master_vol", Tools.Audio.LinearToDecibel(newValue));
         private void UpdateMusicChannelVolume(float newValue) => this.MasterMixer.SetFloat("music_vol", Tools.Audio.LinearToDecibel(newValue));
         private void UpdateUIChannelVolume(float newValue) => this.MasterMixer.SetFloat("ui_vol", Tools.Audio.LinearToDecibel(newValue));
